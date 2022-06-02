f37db0ebcbbd3ab55340ddf62109e323593a720e
sgw8 <vasylmiskiv@gmail.com>
2022-06-02 10:16:51 +0300

    styled sidnav was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fitness-tracker';
-  openSidenav = false;
 }
